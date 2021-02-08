c6bf03349d0df96de80db2954fafe11f677c835f
dmizyn <KAmilami1!2#>
2021-02-08 22:27:59 +0100

    Update

--- src/test/java/com/infraDemo/demo/controller/UserControllerTest.java
+++ src/test/java/com/infraDemo/demo/controller/UserControllerTest.java
@@ -2,8 +2,10 @@ package com.infraDemo.demo.controller;
 
 import com.infraDemo.demo.entity.User;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
